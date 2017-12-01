f6bdd91dcf082ed8c9dc1173a2ccd5da57960365
ckrountree <jmail0207@gmail.com>
2017-12-01 11:55:20 -0800

    pass all e2e tests

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./lib/app');
 const http = require('http');
-const port = process.env.PORT || 3001;
-require('./lib/setup-mongoose');
+const port = process.env.PORT || 3002;
+const connect = require('./lib/connect');
+
+connect();
 
 const server = http.createServer(app);
 server.listen(port, () => {
