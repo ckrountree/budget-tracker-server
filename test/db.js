ec621f423dc825b50e202925932376ed08013940
ckrountree <jmail0207@gmail.com>
2017-12-08 11:32:00 -0800

    add dbDrop

--- test/db.js
+++ test/db.js
@@ -1,6 +1,21 @@
-const connect = require('../lib/connect');
+const connect = require('../lib/utils/connect');
 const url = 'mongodb://localhost:27017/categories-test';
 const mongoose = require('mongoose');
 
 before(() => connect(url));    
 after(() => mongoose.connection.close());
+
+
+module.exports = {
+    drop(connection) {
+        return () => {
+            return new Promise((resolve, reject) => {
+                const drop = () => connection.db.dropDatabase((error, value) => {
+                    error ? reject(error) : resolve(value);
+                });
+                if (connection.readyState === 1) drop();
+                else connect.on('open', drop);
+            });
+        };
+    }
+};
